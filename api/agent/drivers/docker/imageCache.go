9344945f65422b36763f45b5ac82e62ff0480c47
Eric Fode <eric.fode@oracle.com>
2018-09-10 13:51:21 -0700

    added more docs

--- api/agent/drivers/docker/imageCache.go
+++ api/agent/drivers/docker/imageCache.go
@@ -20,14 +20,17 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// Cache is an LRU cache, safe for concurrent access.
+// Cache is a list of Entries. Stored as type EntryByAge so that sort works.
+// The mutex is present to control access access the list of images and their locks
+// The Cache may be used in concurrent contexts
 type Cache struct {
-	totalSize int64
-	mu        sync.Mutex
-	cache     EntryByAge
-	maxSize   int64
+	mu      sync.Mutex
+	cache   EntryByAge
+	maxSize int64
 }
 
+// Entry contains when the image was last used, how many times the image has been used,
+// The image metadata, and a set of tokens that are locking the image in question.
 type Entry struct {
 	lastUsed time.Time
 	locked   map[*interface{}]*interface{}
@@ -35,17 +38,20 @@ type Entry struct {
 	image    d.APIImages
 }
 
+// The score is the time since last use divided by the number of total uses.
 func (e Entry) Score() int64 {
 	age := time.Now().Sub(e.lastUsed)
 	return age.Nanoseconds() / e.uses
 }
 
+// Type to wrap the Entry list so that sort works.
 type EntryByAge []Entry
 
 func (a EntryByAge) Len() int           { return len(a) }
 func (a EntryByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a EntryByAge) Less(i, j int) bool { return a[i].Score() < a[j].Score() }
 
+// NewEntry constructs a an entry from a docker d.APIImages object.
 func NewEntry(value d.APIImages) Entry {
 	return Entry{
 		lastUsed: time.Now(),
@@ -62,12 +68,15 @@ func NewCache(maxSize int64) *Cache {
 	}
 }
 
+// Public method for checking to see if an image in in the list.
+// Equlivilance is determined by image.ID. This method locks.
 func (c *Cache) Contains(value d.APIImages) bool {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	return c.contains(value)
 }
 
+// Helper for contains that does not lock. Used by other imageCache methods internally.
 func (c *Cache) contains(value d.APIImages) bool {
 	for _, i := range c.cache {
 		if i.image.ID == value.ID {
@@ -77,12 +86,15 @@ func (c *Cache) contains(value d.APIImages) bool {
 	return false
 
 }
+
+// Marks a image by ID as having been used.
 func (c *Cache) Mark(ID string) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	return c.mark(ID)
 }
 
+// Internal method for finding the image and adding to it's used count as well as it's lastUsed time.
 func (c *Cache) mark(ID string) error {
 	for idx, i := range c.cache {
 		if i.image.ID == ID {
@@ -95,6 +107,7 @@ func (c *Cache) mark(ID string) error {
 	return errors.New("Image not found in cache")
 }
 
+// Remove deletes an image from the list. Also grabs a smaller portion of the slice.
 func (c *Cache) Remove(value d.APIImages) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -111,12 +124,14 @@ func (c *Cache) Remove(value d.APIImages) error {
 	return errors.New("Image not found in cache")
 }
 
+// Adds a token to the list of locks on this image by id.
 func (c *Cache) Lock(ID string, key interface{}) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	return c.lock(ID, key)
 }
 
+// Internal method for Lock. Contains no locks.
 func (c *Cache) lock(ID string, key interface{}) error {
 	for _, i := range c.cache {
 		if i.image.ID == ID {
@@ -127,6 +142,7 @@ func (c *Cache) lock(ID string, key interface{}) error {
 	return errors.New("Image not found in cache")
 }
 
+// Check to see if an image by id as any locks on it.
 func (c *Cache) Locked(ID string) (bool, error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -142,6 +158,7 @@ func (c *Cache) locked(ID string) (bool, error) {
 	return false, errors.New("Image not found in cache")
 }
 
+// Removes a token from an image by ID's lock set.
 func (c *Cache) Unlock(ID string, key interface{}) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -169,16 +186,19 @@ func (c *Cache) Add(value d.APIImages) {
 	c.cache = append(c.cache, NewEntry(value))
 }
 
+// Checks the total size of the docker data dir.
 func (c *Cache) TotalSize() int64 {
 	return 0
 }
 
+// Returns true if there is to much space consumed.
 func (c *Cache) OverFilled() bool {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	return c.TotalSize() > c.maxSize
 }
 
+// Checks for evictable images ordered by score.
 func (c *Cache) Evictable() EntryByAge {
 	c.mu.Lock()
 	defer c.mu.Unlock()
